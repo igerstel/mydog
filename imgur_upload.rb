f9a4c15eaecfa18a474cc72b0e5bd757c3fd610b
Ian Gerstel <iangerstel@gmail.com>
2012-10-04 15:03:24 -0400

    writing test Ruby script.

---
+++ imgur_upload.rb
@@ -0,0 +1,28 @@
+require 'rubygems'
+require 'rest_client'
+require 'open-uri'
+# require 'nethttp'
+# require 'json'
+
+def imgur(key, file_path)
+  url        = "http://imgur.com/api/upload.json"
+  data       = {
+    :key     => key, :image => open(file_path)
+  }
+
+  response   = RestClient.post(url, data)
+  json       = JSON.parse(response.body)["rsp"]["image"] rescue nil
+
+puts response
+puts "----------------"
+puts json
+
+end
+
+key = File.open('imgur_api.txt').readline.to_s
+
+pic_url = "https://sphotos-a.xx.fbcdn.net/hphotos-prn1/562932_515451068470391_1542759738_n.jpg"
+
+imgur(key, pic_url)
+
+
